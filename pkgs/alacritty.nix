06bcd4dfdd210bb28d87d7d51216aeebc1a6e42e
Aaron Janse <aaron@ajanse.me>
2021-03-07 02:38:35 -0800

    refactor alacritty, update colors for kakoune

--- pkgs/alacritty.nix
+++ pkgs/alacritty.nix
@@ -1,8 +1,38 @@
 { alacritty, symlinkJoin, writeText, makeWrapper, theme }:
 
-let config = writeText "alacritty.yaml" ''
-  {"font":{"normal":{"family":"JuliaMono"},"size":10.0},"colors":{"bright":{"black":"0x555555","blue":"#bd93f9","cyan":"#8be9fd","green":"#50fa7b","magenta":"#ff79c6","red":"#ff5555","white":"0xffffff","yellow":"#f1fa8c"},"normal":{"black":"0x000000","blue":"#bd93f9","cyan":"#8be9fd","green":"#50fa7b","magenta":"#ff79c6","red":"#ff5555","white":"0xbbbbbb","yellow":"#f1fa8c"},"primary":{"background":"${theme.background}","foreground":"#f8f8f2"}},"env":{"TERM":"xterm-256color"}}
-''; in
+let config = writeText "alacritty.yaml" (builtins.toJSON {
+  colors = {
+    bright = {
+      black = "#555555";
+      blue = "#bd93f9";
+      cyan = "#8be9fd";
+      green = "#50fa7b";
+      magenta = "#ff79c6";
+      red = "#ff5555";
+      white = "#ffffff";
+      yellow = "#f1fa8c";
+    };
+    normal = {
+      black = "#555555";
+      blue = "#bd93f9";
+      cyan = "#8be9fd";
+      green = "#50fa7b";
+      magenta = "#ff79c6";
+      red = "#ff5555";
+      white = theme.backgroundSecondary;
+      yellow = "#f1fa8c";
+    };
+    primary = {
+      background = theme.background;
+      foreground = "#f8f8f2";
+    };
+  };
+  env.TERM = "xterm-256color";
+  font = {
+    normal.family = "JuliaMono";
+    size = 10;
+  };
+}); in
 symlinkJoin
 {
   name = "alacritty-custom";
