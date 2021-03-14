f2b593edc3022d19e31a9ed23e5a89bd9b296cf5
Aaron Janse <aaron@ajanse.me>
2021-03-13 17:43:20 -0800

    add python lsp libraries

--- profile.nix
+++ profile.nix
@@ -17,7 +17,18 @@ rec {
       fzf
       git
       jq
-      python3
+      (python3.withPackages
+        (ps:
+          with ps; [
+            black
+            flake8
+            autopep8
+            pep8
+            pyls-mypy
+            mypy
+            setuptools
+            virtualenv
+          ]))
       ripgrep
       tree
       unzip
@@ -32,104 +43,105 @@ rec {
   # Installed on personal systems with a GUI
   gui = pkgs.buildEnv
     {
-    name = "ajanse-env-common";
-    # Include all package in `common` above
-    paths = [ common ] ++ (with pkgs; [
-      age
-      alacritty
-      anki
-      audacity
-      autorandr
-      binutils
-      blueman
-      cachix
-      calibre
-      chromium
-      cmake
-      cmatrix
-      cowsay
-      cryfs
-      cryptsetup
-      dbeaver
-      direnv
-      discord
-      docker
-      docker-compose
-      element-desktop
-      ffmpeg-full
-      firefox-beta-bin
-      flameshot
-      foliate
-      gcc
-      git-annex
-      git-annex-remote-rclone
-      git-remote-gcrypt
-      gitAndTools.gh
-      gnome3.gnome-screenshot
-      gnome3.nautilus
-      gnumake
-      go
-      gopls
-      hexyl
-      htop
-      imagemagick
-      inkscape
-      inotify-tools
-      ipfs
-      ipfs-cluster
-      jetbrains.idea-community
-      julia
-      kak-lsp
-      kakoune
+      name = "ajanse-env-common";
+      # Include all package in `common` above
+      paths = [ common ] ++ (with pkgs; [
+        age
+        alacritty
+        anki
+        audacity
+        autorandr
+        binutils
+        blueman
+        cachix
+        calibre
+        chromium
+        cmake
+        cmatrix
+        cowsay
+        cryfs
+        cryptsetup
+        dbeaver
+        direnv
+        discord
+        docker
+        docker-compose
+        element-desktop
+        ffmpeg-full
+        firefox-beta-bin
+        flameshot
+        foliate
+        gcc
+        git-annex
+        git-annex-remote-rclone
+        git-remote-gcrypt
+        gitAndTools.gh
+        gnome3.gnome-screenshot
+        gnome3.nautilus
+        gnumake
+        go
+        gopls
+        hexyl
+        htop
+        imagemagick
+        inkscape
+        inotify-tools
+        ipfs
+        ipfs-cluster
+        jetbrains.idea-community
+        julia
+        kak-lsp
+        kakoune
         kakounePlugins.kak-fzf
-      kitty
-      krita
-      kubectl
-      kubernetes-helm
-      libuuid.dev
-      litecli
-      lolcat
-      lsof
-      matrix-synapse
-      maven3
-      mosh
-      mullvad-vpn
-      multimc
-      nheko
-      nix-direnv
-      nixpkgs-fmt
-      nodejs
-      nodePackages.insect
-      nodePackages.typescript
-      okular
-      openjdk
-      pandoc
-      pinentry-gtk2
-      pkgconfig
-      powertop
-      pstree
-      qt514.full
-      quaternion
-      rclone
-      rustup
-      signal-desktop
-      spotify
-      sqlite
-      sqlitebrowser
-      tailscale
-      telnet
-      tldr
-      tomb
-      toot
-      vscode
-      w3m
-      wireguard
-      xclip
-      xwiimote
-      yarn
-      youtube-dl
-      zoom-us
-    ]);
-    extraOutputsToInstall = [ "man" "doc" ];
-  };
+        kitty
+        krita
+        kubectl
+        kubernetes-helm
+        libuuid.dev
+        litecli
+        lolcat
+        lsof
+        matrix-synapse
+        maven3
+        mosh
+        mullvad-vpn
+        multimc
+        nheko
+        nix-direnv
+        nixpkgs-fmt
+        nodejs
+        nodePackages.insect
+        nodePackages.typescript
+        okular
+        openjdk
+        pandoc
+        pinentry-gtk2
+        pkgconfig
+        powertop
+        pstree
+        python-language-server
+        qt514.full
+        quaternion
+        rclone
+        rustup
+        signal-desktop
+        spotify
+        sqlite
+        sqlitebrowser
+        tailscale
+        telnet
+        tldr
+        tomb
+        toot
+        vscode
+        w3m
+        wireguard
+        xclip
+        xwiimote
+        yarn
+        youtube-dl
+        zoom-us
+      ]);
+      extraOutputsToInstall = [ "man" "doc" ];
+    };
 }
