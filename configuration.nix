e174a249e94f69ccfa38b424b93ebb04e81b61c1
Aaron Janse <aaron@ajanse.me>
2021-03-13 17:46:02 -0800

    update kernel (for now)

--- configuration.nix
+++ configuration.nix
@@ -69,34 +69,34 @@ in
 
   boot.loader.grub.copyKernels = true;
 
-  boot.kernelPackages =
-    let
-      linux_pkg =
-        { stdenv, lib, buildPackages, fetchurl, perl, buildLinux, modDirVersionArg ? null, ... } @ args:
-          with lib;
-          buildLinux (
-            args // rec {
-              version = "5.3.16";
-              modDirVersion =
-                if (modDirVersionArg == null)
-                then concatStringsSep "." (take 3 (splitVersion "${version}.0"))
-                else modDirVersionArg;
-              kernelPatches = [{
-                name = "fix-display";
-                patch = pkgs.fetchpatch {
-                  url = "https://bugs.freedesktop.org/attachment.cgi?id=144765";
-                  sha256 = "sha256-Fc6V5UwZsU6K3ZhToQdbQdyxCFWd6kOxU6ACZKyaVZo=";
-                };
-              }];
-              src = fetchurl {
-                url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-                sha256 = "19asdv08rzp33f0zxa2swsfnbhy4zwg06agj7sdnfy4wfkrfwx49";
-              };
-            } // (args.argsOverride or { })
-          );
-      linux = pkgs.callPackage linux_pkg { };
-    in
-    pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux);
+  # boot.kernelPackages =
+  #   let
+  #     linux_pkg =
+  #       { stdenv, lib, buildPackages, fetchurl, perl, buildLinux, modDirVersionArg ? null, ... } @ args:
+  #         with lib;
+  #         buildLinux (
+  #           args // rec {
+  #             version = "5.3.16";
+  #             modDirVersion =
+  #               if (modDirVersionArg == null)
+  #               then concatStringsSep "." (take 3 (splitVersion "${version}.0"))
+  #               else modDirVersionArg;
+  #             kernelPatches = [{
+  #               name = "fix-display";
+  #               patch = pkgs.fetchpatch {
+  #                 url = "https://bugs.freedesktop.org/attachment.cgi?id=144765";
+  #                 sha256 = "sha256-Fc6V5UwZsU6K3ZhToQdbQdyxCFWd6kOxU6ACZKyaVZo=";
+  #               };
+  #             }];
+  #             src = fetchurl {
+  #               url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
+  #               sha256 = "19asdv08rzp33f0zxa2swsfnbhy4zwg06agj7sdnfy4wfkrfwx49";
+  #             };
+  #           } // (args.argsOverride or { })
+  #         );
+  #     linux = pkgs.callPackage linux_pkg { };
+  #   in
+  #   pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux);
 
   /* Maintenance */
 
@@ -341,10 +341,10 @@ in
   #   enable = true;
   #   enableExtensionPack = false; # takes a long time to build
   # };
-  # virtualisation.docker = {
-  #   enable = true;
-  #   storageDriver = "zfs";
-  # };
+  virtualisation.docker = {
+    enable = true;
+    storageDriver = "zfs";
+  };
 
   system.stateVersion = "19.03";
 }
