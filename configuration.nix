27862d6c1f2233c11599312fc903aadb804b1bb3
Aaron Janse <aaron@ajanse.me>
2021-06-27 19:54:14 -0700

    update kernel patch code

--- configuration.nix
+++ configuration.nix
@@ -85,34 +85,22 @@ in
 
   boot.loader.grub.copyKernels = true;
 
-  # boot.kernelPackages =
-  #   let
-  #     linux_pkg =
-  #       { stdenv, lib, buildPackages, fetchurl, perl, buildLinux, modDirVersionArg ? null, ... } @ args:
-  #         with lib;
-  #         buildLinux (
-  #           args // rec {
-  #             version = "5.3.16";
-  #             modDirVersion =
-  #               if (modDirVersionArg == null)
-  #               then concatStringsSep "." (take 3 (splitVersion "${version}.0"))
-  #               else modDirVersionArg;
-  #             kernelPatches = [{
-  #               name = "fix-display";
-  #               patch = pkgs.fetchpatch {
-  #                 url = "https://bugs.freedesktop.org/attachment.cgi?id=144765";
-  #                 sha256 = "sha256-Fc6V5UwZsU6K3ZhToQdbQdyxCFWd6kOxU6ACZKyaVZo=";
-  #               };
-  #             }];
-  #             src = fetchurl {
-  #               url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-  #               sha256 = "19asdv08rzp33f0zxa2swsfnbhy4zwg06agj7sdnfy4wfkrfwx49";
-  #             };
-  #           } // (args.argsOverride or { })
-  #         );
-  #     linux = pkgs.callPackage linux_pkg { };
-  #   in
-  #   pkgs.recurseIntoAttrs (pkgs.linuxPackagesFor linux);
+  # boot.kernelPatches = let hashes = [
+  #   "sha256-ffrN0y5Us/VJ1v6JObfkSVD4PCm23wueI0JXjoRcOao="
+  #   "sha256-mh9MgRtLp4+sXjM6GHkAkyyvp5DP3yTxl+d/kJ3+KJQ="
+  #   "sha256-1Jy1uv9x1c06/auY1aHN0SuyOsbIF1IcaK8/PuGmX8Q="
+  #   "sha256-QE3NfIg60RxV528AbpNAcxIdToplRNm5DgMNPn6AjHQ="
+  #   "sha256-zyL1g2oNiTzYfS5qUOehduGfzFfi2WIVLCQMg+XGBwE="
+  #   "sha256-f79tof0O5HBFwLbddR9FOqqLsqGsHqMZM45uYZNcoec="
+  #   "sha256-JD0bi4sjsI9qRpHXxcPlGpiiwXMu2oPm/HJU8iQI1ag="
+  #   "sha256-V+AGk7FGNiidd22ektlGCvSh3Wo2bRrUWp6qgGmsKPI="
+  # ]; in builtins.genList (i: {
+  #   name = "fuse_passthrough ${toString (i+1)}/8";
+  #   patch = pkgs.fetchpatch {
+  #     url = "https://lore.kernel.org/lkml/20210125153057.3623715-${toString (i+2)}-balsini@android.com/raw";
+  #     sha256 = builtins.elemAt hashes i;
+  #   };
+  # }) 8;
 
   /* Maintenance */
 
